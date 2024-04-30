880191b2744f8ac97239f4d0675a6ba9a2c01d6d
Minoto Hosigiri <89182274+mhosigiri@users.noreply.github.com>
2024-04-30 23:12:33 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@DrTInheritanceLessonCOSC1437/includes.h
@@ -0,0 +1,21 @@
+#define _USE_MATH_DEFINES // this must appear first in file  to use M_PI as
+                          // constant
+#include <cmath>
+#include <cstdlib> //for system("pause") and system("cls")
+#include <ctime>   //to perform date/time calculations
+#include <fstream> //for Read/Write objects in C++
+#include <iomanip>
+#include <ios> // Provides ios_base::failure
+#include <iostream>
+#include <sstream>  //for string manipulation and comparison
+#include <string>   //for string manipulation
+#include <unistd.h> //for using sleep() in umbut linux
+//#include<windows.h>//for using various dos commands Windows
+//#include<conio.h>//for using color and/or "sleep" command Windows
+using namespace std;
+
+// important .h file
+#include "Color_in_CPP.h"
+#include "Input_Validation_Extended.h"
+#include "Shape_Classes.h"
+#include "Shape_Functions.h"
---
+++ .ccls-cache/@home@runner@DrTInheritanceLessonCOSC1437/main.cpp
@@ -0,0 +1,43 @@
+/*
+        Dr. Tyson McMillan Working with
+        Source URL: https://www.tutorialspoint.com/cplusplus/cpp_inheritance.htm
+        updated: 4-10-2017, 4-9-2018, 10-30-2018, 4-15-2020 by Dr. Tyson
+McMillan
+        //Extended to have a custom on-demand menu.
+Expand Dr_T's Working Solution
+
+1. Fork Off of: https://repl.it/@TysonMcMillan/DrTInheritanceLessonCOSC1437
+(Links to an external site.)
+
+//Dr_T Challenge - Pair Programming
+1. Expand this program to accept input to Draw a Diamond Pattern
+2. Expand this program to accept input to Draw an Oval Pattern
+
+EXTRA (new)
+
+Extend this program for a Trapezoid print and Menu Item
+
+Extend this program for a Pyramid print and Menu Item
+
+Extend the Program to print 1 of these cool Shapes:
+https://visme.co/blog/cool-shapes/ (Links to an external site.)
+
+
+3. Share your solution with your team member.
+4. Both upload your share URL to Extra Credit Item 15 Participation Extra Credit
+4. Complete the attendance tracker.
+*/
+
+#include "includes.h" //all the .h includes of program
+
+int main() {
+  char menuOption = '\0'; // int main local menu option
+
+  do {
+    menuOption = showMenu(menuOption);              // function call
+  } while (menuOption != 'e' && menuOption != 'E'); // DeMorgan's Law
+
+  cout << "\nGoodbye" << endl;
+
+  return 0;
+}
